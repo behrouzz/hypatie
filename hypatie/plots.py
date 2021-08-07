0b381dfc959277c2699e926b198c4bab647d4c09
Behrouz <behrouz.safari@gmail.com>
2021-08-07 12:30:00 +0200

    pandas in plots.py

--- hypatie/plots.py
+++ hypatie/plots.py
@@ -4,6 +4,7 @@ Module plot
 This module supplies several functions for plotting reasons.
 """
 import numpy as np
+import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 from collections.abc import Iterable
