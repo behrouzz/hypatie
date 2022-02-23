235a3902b89612ab225fa31907a8a9058b9ed06c
Behrouz <behrouz.safari@gmail.com>
2022-02-23 01:15:08 +0100

    add RAhms, DECdms, RAdeg, DECdeg

--- hypatie/transform.py
+++ hypatie/transform.py
@@ -9,7 +9,7 @@ from collections.abc import Iterable
 import re
 
 
-class RA:
+class RAhms:
     def __init__(self, h=0, m=0, s=0):
         self.h = h
         self.m = m
@@ -21,7 +21,7 @@ class RA:
         self.rad = self.deg * (3.141592653589793/180)
         
 
-class DEC:
+class DECdms:
     def __init__(self, sign='+', d=0, m=0, s=0):
         self.sign = sign
         sgn = -1 if sign=='-' else 1
@@ -34,6 +34,27 @@ class DEC:
         self.deg = self.degrees
         self.rad = self.deg * (3.141592653589793/180)
 
+
+class RAdeg:
+    def __init__(self, ra):
+        self.rad = ra * (3.141592653589793/180)
+        self.h = int(ra/15)
+        self.m = int(((ra/15)-self.h)*60)
+        self.s = ((((ra/15)-self.h)*60)-self.m)*60
+        self.str = str(self.h)+'h' + str(self.m)+'m' + str(self.s)+'s'
+        
+
+class DECdeg:
+    def __init__(self, dec):
+        self.rad = dec * (3.141592653589793/180)
+        self.sign = '-' if dec<0 else '+'
+        dec = abs(dec)
+        self.d = int(dec)
+        self.m = abs(int((dec-self.d)*60))
+        self.s = (abs((dec-self.d)*60)-self.m)*60
+        self.str = self.sign + str(self.d)+'d' + \
+                   str(self.m)+'m' + str(self.s)+'s'
+        
 
 def _time(t):
     if isinstance(t, datetime):
