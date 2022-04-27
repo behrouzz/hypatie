28815b81012664e969bd4c72a8bf5c213d1d9bf8
Behrouz Safari <behrouz.safari@gmail.com>
2022-04-27 22:36:51 +0200

    find_packages for data subpackage

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="hypatie",
-    version="2.16.0",
+    version="2.16.1",
     author="Behrouz Safari",
     author_email="behrouz.safari@gmail.com",
     description="A python package for astronomical calculations",
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=["hypatie"],
+    packages = find_packages(),
     include_package_data=True,
     install_requires=["numpy", "scipy", "matplotlib","pandas", "requests"],
     python_requires='>=3.4',
