cdc2c65937589b44c6ff58012d2b4e5e218728b3
Behrouz <behrouz.safari@gmail.com>
2022-06-04 12:38:52 +0200

    fix bug in solar_system

--- hypatie/__init__.py
+++ hypatie/__init__.py
@@ -5,4 +5,4 @@ from .transform import radec_to_altaz, sph2car, car2sph
 from .simbad import sql2df
 from .time import datetime_to_jd, jd_to_datetime, utc2tdb, tdb2utc
 
-__version__ = "2.17.0"
+__version__ = "2.17.1"
